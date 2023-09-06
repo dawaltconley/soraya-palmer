2c388594ad65dc8723a722d9a62898ce1eab3d6b
Dylan Awalt-Conley <web@dylan.ac>
2023-09-06 19:17:41 -0400

    add utils for fetching url metadata

---
+++ src/lib/url-metadata.ts
@@ -0,0 +1,19 @@
+import type { MqlResponse } from '@microlink/mql'
+import mql from '@microlink/mql'
+import { toUrl } from '@lib/utils'
+
+export const getMetadata = async (
+  urlString: string | URL,
+): Promise<MqlResponse['data']> => {
+  const url = toUrl(urlString)
+  if (!url) throw new Error(`Couldn't parse url: ${urlString}`)
+
+  console.log('fetching', url.href)
+  const { status, data } = await mql(url.href, { meta: true })
+  if (status !== 'success')
+    throw new Error(`Error fetching metadata for ${url.href}: ${status}`)
+
+  console.log('fetched', status)
+
+  return data
+}
