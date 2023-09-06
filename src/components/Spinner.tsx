7cffd7ad7344e37e5bd8e0f39a9462084f14d857
Dylan Awalt-Conley <web@dylan.ac>
2023-09-06 00:28:50 -0400

    add basic spinner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,20 @@
+import type { IconProps } from './Icon'
+import Icon from './Icon'
+import clsx from 'clsx'
+import { faSpinnerThird as faLoading } from '@fortawesome/pro-regular-svg-icons/faSpinnerThird'
+
+export default function Spinner({
+  className,
+  class: className2,
+  ...props
+}: Omit<IconProps, 'icon'>) {
+  return (
+    <Icon
+      icon={faLoading}
+      width="1em"
+      height="1em"
+      class={clsx('fa-spin', className, className2)}
+      {...props}
+    />
+  )
+}
