187cba3a90b75df9d82e185e8914c0e5a3346623
Dylan Awalt-Conley <web@dylan.ac>
2023-09-06 20:37:12 -0400

    remove bg from article preview (simpler styling)

--- src/components/ArticlePreview.tsx
+++ src/components/ArticlePreview.tsx
@@ -38,12 +38,12 @@ export default function ArticlePreview({
   const H = 'h3'
 
   return (
-    <div className="relative flex flex-col bg-white font-serif text-base">
+    <div className="relative flex flex-col font-serif text-base">
       <div className="layer-children aspect-video shrink-0">
         <img className="object-cover" src={image.url} alt="" />
       </div>
-      <div className="h-full border-y-4 border-gray-100 p-2">
-        <H className="font-display text-2xl font-bold">
+      <div className="h-full">
+        <H className="mt-2 font-display text-2xl font-bold">
           <a href={url.toString()} className="pseudo-fill-parent">
             {title}
           </a>
