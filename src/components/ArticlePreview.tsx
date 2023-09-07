4f2cb3d081ca19aae39b3fc93b084227b821deb2
Dylan Awalt-Conley <web@dylan.ac>
2023-09-06 20:34:41 -0400

    updated article preview styles

--- src/components/ArticlePreview.tsx
+++ src/components/ArticlePreview.tsx
@@ -38,17 +38,19 @@ export default function ArticlePreview({
   const H = 'h3'
 
   return (
-    <div className="relative font-serif text-base">
-      <div className="layer-children aspect-video">
+    <div className="relative flex flex-col bg-white font-serif text-base">
+      <div className="layer-children aspect-video shrink-0">
         <img className="object-cover" src={image.url} alt="" />
       </div>
-      <H className="mt-2 font-display text-2xl font-bold">
-        <a href={url.toString()} className="pseudo-fill-parent">
-          {title}
-        </a>
-      </H>
-      {publisher && <p className="italic text-gray-500">{publisher}</p>}
-      {description && <p className="mt-2">{description}</p>}
+      <div className="h-full border-y-4 border-gray-100 p-2">
+        <H className="font-display text-2xl font-bold">
+          <a href={url.toString()} className="pseudo-fill-parent">
+            {title}
+          </a>
+        </H>
+        {publisher && <p className="italic text-gray-500">{publisher}</p>}
+        {description && <p className="mt-2">{description}</p>}
+      </div>
     </div>
   )
 }
