e2267605e2003c8fb8e22cd6205cdc4fc881a2d0
Dylan Awalt-Conley <web@dylan.ac>
2023-09-21 14:44:23 -0400

    fix sass import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 
 import path from 'node:path'
-import sass from 'sass'
+import * as sass from 'sass'
 import mkTwFunctions from 'sass-tailwind-functions'
 import react from '@astrojs/react'
 
