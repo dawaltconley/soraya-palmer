424dfd82a0f5679ab654585acac76ff83f507f7c
Dylan Awalt-Conley <web@dylan.ac>
2023-09-22 10:34:28 -0400

    configure tina cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,14 @@ import path from 'node:path'
 import * as sass from 'sass'
 import mkTwFunctions from 'sass-tailwind-functions'
 import react from '@astrojs/react'
+import tina from 'astro-tina'
 
 const twFunctions = mkTwFunctions(sass, path.resolve('./tailwind.config.cjs'))
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  integrations: [react()],
+  integrations: [react(), tina()],
   vite: {
     css: {
       preprocessorOptions: {
