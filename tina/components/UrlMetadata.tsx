53c5e70f6f4b61a897e0f885e90a6d193616a0ab
Dylan Awalt-Conley <web@dylan.ac>
2023-09-23 22:06:36 -0400

    add custom tina field component for url metadata

--- tina/components/UrlMetadata.tsx
+++ tina/components/UrlMetadata.tsx
@@ -1,8 +1,9 @@
 import type { MqlResponse } from '@microlink/mql'
 import mql from '@microlink/mql'
-import React, { useState, useEffect, useRef } from 'react'
-import { Input, FieldLabel, GroupLabel, wrapFieldsWithMeta } from 'tinacms'
-import { memoize } from 'lodash'
+import React, { useState, useRef } from 'react'
+import type { InputProps } from 'tinacms'
+import { wrapFieldsWithMeta, Input, LoadingDots } from 'tinacms'
+import { get, memoize } from 'lodash'
 
 const getMetadata = memoize(
   async (urlString: string | URL): Promise<MqlResponse['data']> => {
@@ -13,114 +14,68 @@ const getMetadata = memoize(
       throw new Error(`Couldn't parse url: ${urlString}`)
     }
 
-    console.log('fetching...', urlString)
     const { status, data } = await mql(url.href, { meta: true })
     if (status !== 'success')
       throw new Error(`Error fetching metadata for ${url.href}: ${status}`)
-    console.log('fetched', urlString)
 
     return data
   },
 )
 
-// const getMetadataDebounced = (urlString: string | URL):
+export interface UrlMetadataProps {
+  metadataFields: Record<string, string>
+}
 
-const UrlMetadata = ({ field, input, meta }: any) => {
-  // console.log({ input, meta })
+export const UrlMetadata = wrapFieldsWithMeta<InputProps, UrlMetadataProps>(
+  ({ field, input, form }) => {
+    if (!('form' in window)) {
+      ;(window as any).form = form
+    }
 
-  const [url, setUrl] = useState('')
-  const [metadata, setMetadata] = useState<MqlResponse['data']>()
-  const [error, setError] = useState<Error | null>(null)
-  const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState<Error | null>(null)
+    const [isLoading, setIsLoading] = useState(false)
 
-  const debounce = useRef<number>()
-  const onUrlChange = (url: string): void => {
-    window.clearTimeout(debounce.current)
-    debounce.current = window.setTimeout(() => {
-      getMetadata(url)
-        .then((data) => {
-          setMetadata(data)
-          input.onChange({
-            ...data,
-            url,
-            foo: 'bar',
-            bar: 'baz',
+    const debounce = useRef<number>()
+    const onUrlChange = (url: string): void => {
+      setIsLoading(true)
+      window.clearTimeout(debounce.current)
+      debounce.current = window.setTimeout(() => {
+        getMetadata(url)
+          .then((metadata) => {
+            setError(null)
+            if (metadata.url) input.onChange(metadata.url)
+            form.batch(() => {
+              Object.entries(field.metadataFields).forEach(
+                ([field, property]) => {
+                  form.change(field, get(metadata, property, ''))
+                },
+              )
+            })
           })
-          setError(null)
-        })
-        .catch(setError)
-        .finally(() => setIsLoading(false))
-    }, 1200)
-  }
+          .catch(setError)
+          .finally(() => setIsLoading(false))
+      }, 1200)
+    }
 
-  console.log({ metadata })
-
-  return (
-    <fieldset className="space-y-2">
-      <legend>{field.label}</legend>
-      <div>{JSON.stringify({ error, isLoading })}</div>
-      {/*  <FieldLabel name={field.name}>{field.label}</FieldLabel>  */}
-      <Input
-        {...input}
-        value={url}
-        onChange={(e: any) => {
-          // console.log(e)
-          const value = e.target.value
-          setUrl(value)
-          onUrlChange(value)
-        }}
-      />
-      {
-        // <Input
-        // onChange={(e: any) => {
-        //   // console.log(e)
-        //   const value = e.target.value
-        //   input.onChange({
-        //     url,
-        //     foo: value,
-        //   })
-        //   }}
-        // />
-      }
-    </fieldset>
-  )
-}
-
-export default UrlMetadata
-
-export const urlMetadataFields = {
-  type: 'object',
-  name: 'metadata',
-  label: 'Metadata',
-  ui: {
-    component: UrlMetadata,
+    return (
+      <>
+        <div className="relative">
+          <Input
+            {...input}
+            // value={input.value}
+            error={error}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              input.onChange(e.target.value)
+              onUrlChange(e.target.value)
+            }}
+          />
+          {isLoading && (
+            <div className="absolute bottom-0 right-2 top-0 flex flex-col justify-center">
+              <LoadingDots color="rgb(67 62 82)" />
+            </div>
+          )}
+        </div>
+      </>
+    )
   },
-  fields: [
-    {
-      type: 'string',
-      name: 'url',
-      label: 'URL',
-      required: 'true',
-    },
-    {
-      type: 'string',
-      name: 'title',
-      label: 'Title',
-    },
-    {
-      type: 'image',
-      name: 'image',
-      label: 'Image',
-    },
-    {
-      type: 'string',
-      name: 'publisher',
-      label: 'Publisher',
-    },
-    {
-      type: 'string',
-      name: 'description',
-      label: 'Description',
-    },
-  ],
-}
+)
