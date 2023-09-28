882542e6525b43098167c5f3c343cbae0fd257db
Dylan Awalt-Conley <web@dylan.ac>
2023-09-28 16:07:55 -0400

    WIP press page styling

--- src/components/PressGrid.tsx
+++ src/components/PressGrid.tsx
@@ -1,3 +1,4 @@
+import type { ReactNode } from 'react'
 import type { PressPageQuery } from '@tina/__generated__/types'
 import { useTina, tinaField } from 'tinacms/dist/react'
 import { TinaMarkdown } from 'tinacms/dist/rich-text'
@@ -17,11 +18,13 @@ export default function PressGrid({ query }: PressGridProps) {
       data-tina-field={tinaField(data.pressPage.press)}
     >
       {press.map((p) => {
+        let content: ReactNode
         if (p.__typename === 'PressPagePressArticle' && p.article) {
           const id = p.article._sys.filename
-          const { url, title, image, description, source } = p.article
+          const { url, title, image, source } = p.article
+          const description = <TinaMarkdown content={p.article.description} />
           if (image) {
-            return (
+            content = (
               <ArticlePreview
                 key={id}
                 url={url}
@@ -32,12 +35,13 @@ export default function PressGrid({ query }: PressGridProps) {
               />
             )
           } else {
-            return <div>Oops! missing article image</div>
+            // TODO fix
+            content = <div>Oops! missing article image</div>
           }
         } else if (p.__typename === 'PressPagePressQuote' && p.quote) {
           const id = p.quote._sys.filename
           const { author, book, quote } = p.quote
-          return (
+          content = (
             <Quote
               key={id}
               type="author"
@@ -50,6 +54,7 @@ export default function PressGrid({ query }: PressGridProps) {
         } else {
           return null
         }
+        return <div data-tina-field={tinaField(p)}>{content}</div>
       })}
     </div>
   )
