38eb20226d3f71b7ad72b6b6f1b5597f47d5580c
Dylan Awalt-Conley <web@dylan.ac>
2023-10-26 14:25:52 -0400

    add simpler card style to ImageCard component

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,12 +1,30 @@
 import type { ReactNode, CSSProperties } from 'react'
 import clsx from 'clsx'
 
+/*
+ * Small versions
+ * 1. tile in middle of image
+ * 2. text below image (requires image?)
+ *
+ * Small preview text
+ * 1. use title block
+ * 2. use description block
+ *
+ * Large versions (only difference whether title is present)
+ * 0. no image, text in middle
+ * 1. all text right of image
+ * 2. title below image, description to right
+ */
+
 interface ImageCardProps {
   image?: string
   alt?: string
   url?: string | URL
   children: ReactNode
   title?: ReactNode
+  style?: 'card' | 'tile'
+  styleLarge?: 'card' | 'tile'
+  imgBasis?: string
 }
 
 export default function ImageCard({
@@ -14,49 +32,62 @@ export default function ImageCard({
   alt = '',
   url,
   // title,
+  style = 'card',
+  // styleLarge = style,
+  imgBasis,
   children,
 }: ImageCardProps) {
   const Wrapper = url ? 'a' : 'div'
   return (
     <Wrapper
       href={url?.toString()}
-      className="group relative flex h-full @container/image-card"
+      className="group relative block h-full @container/image-card"
     >
-      {image && (
-        <div
-          className="overlay-before overlay-after before:vignette absolute inset-0 z-0 h-full duration-500 before:z-10 after:z-20 after:bg-amber-300/10 after:duration-[inherit] group-hover:scale-105 group-hover:after:bg-gray-800/20 @2xl:static @2xl:w-1/2 @2xl:flex-grow"
-          style={
-            {
-              '--vignette-opacity': 0.2,
-              '--vignette-spread': '4rem',
-            } as CSSProperties
-          }
-        >
-          <img
-            className="h-full w-full object-cover"
-            src={image}
-            alt={alt}
-            loading="lazy"
-            decoding="async"
-          />
-        </div>
-      )}
-      <div
-        className={clsx(
-          'flex h-full w-full p-8',
-          image ? '@2xl:w-auto @2xl:p-0' : 'bg-amber-300',
+      <div className="flex h-full w-full flex-col @2xl:flex-row">
+        {image && (
+          <div
+            className={clsx(
+              'overlay-before overlay-after before:vignette grow duration-500 before:z-10 after:z-20 after:bg-amber-300/10 after:duration-[inherit] group-hover:scale-105 group-hover:after:bg-gray-800/20 @2xl:w-1/2',
+              { 'relative shrink-0 basis-[40%]': style === 'card' },
+              {
+                'absolute inset-0 z-0 h-full @2xl:relative': style === 'tile',
+              },
+            )}
+            style={
+              {
+                '--vignette-opacity': 0.2,
+                '--vignette-spread': '4rem',
+                flexBasis: imgBasis,
+              } as CSSProperties
+            }
+          >
+            <img
+              className="h-full w-full object-cover"
+              src={image}
+              alt={alt}
+              loading="lazy"
+              decoding="async"
+            />
+          </div>
         )}
-      >
         <div
           className={clsx(
-            'm-auto border-4 border-amber-300 bg-white drop-shadow-xl',
-            {
-              '@2xl:m-0 @2xl:mr-0 @2xl:h-full @2xl:border-y-0 @2xl:border-r-0':
-                image,
-            },
+            'flex w-full min-w-[50%]',
+            { 'h-full p-8': style === 'tile' },
+            image ? '@2xl:h-full @2xl:w-auto @2xl:p-0' : 'bg-amber-300',
           )}
         >
-          {children}
+          <div
+            className={clsx('relative border-amber-300 bg-white', {
+              'w-full': style === 'card',
+              'border-t-4': style === 'card' && image,
+              'm-auto border-4 drop-shadow-xl': style === 'tile',
+              '@2xl:m-0 @2xl:mr-0 @2xl:h-full @2xl:border-y-0 @2xl:border-l-4 @2xl:border-r-0':
+                image,
+            })}
+          >
+            {children}
+          </div>
         </div>
       </div>
     </Wrapper>
