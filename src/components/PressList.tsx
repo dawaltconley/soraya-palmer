ecffd0621e9bdacb9ae227629e21171a68fda630
Dylan Awalt-Conley <web@dylan.ac>
2023-10-27 12:44:25 -0400

    refactor PressList to use CardList

--- src/components/PressList.tsx
+++ src/components/PressList.tsx
@@ -1,45 +1,26 @@
-import type { PressConnectionQuery } from '@tina/__generated__/types'
-import ArticlePreview from './ArticlePreview'
-import { withTinaWrapper } from '@lib/browser/withTinaWrapper'
+import type {
+  PressPageQuery,
+  PressConnectionQuery,
+} from '@tina/__generated__/types'
+import type { TinaData } from '@lib/browser/withTinaWrapper'
+import { useTina } from 'tinacms/dist/react'
 import { isNotEmpty } from '@lib/utils'
-import type { PressPageQuery } from '@tina/__generated__/types'
-import { useTina, tinaField } from 'tinacms/dist/react'
-import { TinaMarkdown } from 'tinacms/dist/rich-text'
+import CardList from './CardList'
 
 export interface PressListProps {
-  exclude: Parameters<typeof useTina<PressPageQuery>>[0]
+  data: TinaData<PressConnectionQuery>
+  exclude: TinaData<PressPageQuery>
 }
 
-export default withTinaWrapper<PressConnectionQuery, PressListProps>(
-  ({ data, exclude: excludeQuery }) => {
-    const exclude = useTina(excludeQuery)
-      .data.pressPage.press.map((p) =>
-        p.__typename === 'PressPagePressArticle' ? p.article?.id : null,
-      )
-      .filter(isNotEmpty)
-    const press =
-      data.pressConnection.edges
-        ?.map((e) => e?.node)
-        .filter((p) => p?.date && exclude.every((id) => id !== p?.id))
-        .filter(isNotEmpty) || []
-    press.sort(
-      (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime(),
+export default function PressList({
+  data,
+  exclude: excludeQuery,
+}: PressListProps) {
+  const exclude = useTina(excludeQuery)
+    .data.pressPage.press.map((p) =>
+      p.__typename === 'PressPagePressArticle' ? p.article?.id : null,
     )
+    .filter(isNotEmpty)
 
-    return (
-      <ul className="grid gap-4 text-lg md:grid-cols-2 lg:grid-cols-1">
-        {press.map((press) => {
-          const { id, image, description, ...props } = press
-          if (!image) return null
-          return (
-            <li key={id} data-tina-field={tinaField(press, 'title')}>
-              <ArticlePreview image={image} {...props}>
-                <TinaMarkdown content={description} />
-              </ArticlePreview>
-            </li>
-          )
-        })}
-      </ul>
-    )
-  },
-)
+  return <CardList data={data} exclude={exclude} />
+}
