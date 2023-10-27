68270a7e655d778961a7ca7b228ea5bd3312832f
Dylan Awalt-Conley <web@dylan.ac>
2023-10-27 12:09:46 -0400

    add basic list to writing page

---
+++ src/components/CardList.tsx
@@ -0,0 +1,47 @@
+import type {
+  WritingConnectionQuery,
+  PressConnectionQuery,
+} from '@tina/__generated__/types'
+import ArticlePreview from './ArticlePreview'
+import { withTinaWrapper } from '@lib/browser/withTinaWrapper'
+import { isNotEmpty } from '@lib/utils'
+import { tinaField } from 'tinacms/dist/react'
+import { TinaMarkdown } from 'tinacms/dist/rich-text'
+
+interface CardListProps {
+  exclude?: string[]
+}
+
+export default withTinaWrapper<
+  PressConnectionQuery | WritingConnectionQuery,
+  CardListProps
+>(({ data, exclude = [] }) => {
+  const connection =
+    'pressConnection' in data ? data.pressConnection : data.writingConnection
+  const cards =
+    connection.edges
+      ?.map((e) => e?.node)
+      .filter((p) => p?.date && exclude.every((id) => id !== p?.id))
+      .filter(isNotEmpty) || []
+  cards.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+
+  return (
+    <ul className="grid gap-4 text-lg md:grid-cols-2 lg:grid-cols-1">
+      {cards.map((card) => {
+        const { id, image, description, ...props } = card
+        if (!image) return null
+        return (
+          <li key={id} data-tina-field={tinaField(card, 'title')}>
+            <ArticlePreview image={image} {...props}>
+              {description && typeof description === 'string' ? (
+                description
+              ) : (
+                <TinaMarkdown content={description} />
+              )}
+            </ArticlePreview>
+          </li>
+        )
+      })}
+    </ul>
+  )
+})
