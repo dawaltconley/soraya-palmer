3a8d3d141787f193c611380f49210680d2cefac6
Dylan Awalt-Conley <web@dylan.ac>
2023-10-26 21:54:24 -0400

    fixup! add remaining press items below fold on press page

--- src/components/PressList.tsx
+++ src/components/PressList.tsx
@@ -1,26 +1,12 @@
-import type { PressPage, PressConnectionQuery } from '@tina/__generated__/types'
+import type { PressConnectionQuery } from '@tina/__generated__/types'
 import ArticlePreview from './ArticlePreview'
 import { withTinaWrapper } from '@lib/browser/withTinaWrapper'
 import { isNotEmpty } from '@lib/utils'
-// import type { ReactNode, CSSProperties } from 'react'
 import type { PressPageQuery } from '@tina/__generated__/types'
 import { useTina, tinaField } from 'tinacms/dist/react'
 import { TinaMarkdown } from 'tinacms/dist/rich-text'
-// import ArticleQuote, { isArticleStyle } from './ArticleQuote'
-// import { AuthorQuote, isQuoteStyle } from './Quote'
-
-// export interface PressListProps {
-//   press: Parameters<typeof useTina<PressConnectionQuery>>[0]
-//   exclude?: Parameters<typeof useTina<PressPageQuery>>[0]
-// }
-//
-// export default function PressList({ press, exclude }: PressListProps) {
-//   const { data } = useTina(press)
-//   const { data: excludeData } useTina(exclude)
-// }
 
 export interface PressListProps {
-  // press: Parameters<typeof useTina<PressConnectionQuery>>[0]
   exclude: Parameters<typeof useTina<PressPageQuery>>[0]
 }
 
