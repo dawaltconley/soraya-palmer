effc5f241f1139f1929d369cf78f9efa0835abe3
Dylan Awalt-Conley <web@dylan.ac>
2023-10-28 23:16:36 -0400

    add type comment

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 const plugin = require('tailwindcss/plugin')
 
+/** @type import('tailwindcss').Config */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
   theme: {
