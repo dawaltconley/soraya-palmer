e1e302a7d68cb1b4757ee52f0bdc3b7c04c4ea54
Dylan Awalt-Conley <web@dylan.ac>
2023-10-29 01:22:51 -0400

    allow defining null images on Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,6 +3,8 @@ import type { ImageCardProps } from './ImageCard'
 import CardBasic from './CardBasic'
 import ImageCard from './ImageCard'
 
-export default function Card(props: CardBasicProps | ImageCardProps) {
+export default function Card(
+  props: (CardBasicProps & { image?: null }) | ImageCardProps,
+) {
   return 'image' in props && props.image ? ImageCard(props) : CardBasic(props)
 }
