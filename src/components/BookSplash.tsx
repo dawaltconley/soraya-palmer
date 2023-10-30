ff9784c6e22207266444f9db9e6ba2fd14944aee
Dylan Awalt-Conley <web@dylan.ac>
2023-10-30 18:03:02 -0400

    minimally complete bookstore select menu

--- src/components/BookSplash.tsx
+++ src/components/BookSplash.tsx
@@ -4,13 +4,15 @@ import type {
   CSSProperties,
 } from 'react'
 import { useState, useEffect, useRef } from 'react'
-import Select from 'react-select'
+// import Select from 'react-select'
 import * as Dropdown from '@radix-ui/react-dropdown-menu'
 import Icon from './Icon'
 import Spinner from './Spinner'
 import clsx from 'clsx'
 import { faCirclePlay } from '@fortawesome/pro-regular-svg-icons/faCirclePlay'
 import { faXmark } from '@fortawesome/pro-regular-svg-icons/faXmark'
+import type { Bookstore } from '@data/bookstores'
+import bookstores from '@data/bookstores'
 
 interface TrailerProps extends ComponentPropsWithoutRef<'video'> {
   play?: boolean
@@ -59,207 +61,109 @@ const Trailer = ({
   )
 }
 
-const bookstoreTags = [
-  'Black owned',
-  'worker/customer owned',
-  'Black women owned',
-] as const
-
-interface Bookstore {
-  name: string
-  link: URL
-  location?: string
-  // tag?: 0 | 1 | 2
-  tag?: number
-  id: number
-}
-
-const bookstores: Bookstore[] = [
-  {
-    name: 'Bookshop.org',
-    link: new URL('https://www.thelitbar.com/'),
-  },
-  {
-    name: 'The Lit. Bar',
-    link: new URL(
-      'https://bookshop.org/p/books/the-human-origins-of-beatrice-porter-other-essential-ghosts-soraya-palmer/18592932',
-    ),
-    location: 'NYC',
-    tag: 2,
-  },
-  {
-    name: 'Cafe con Libros',
-    link: new URL('https://www.cafeconlibrosbk.com/'),
-    location: 'NYC',
-    tag: 2,
-  },
-  {
-    name: 'Bluestockings',
-    link: new URL('https://bluestockings.com/about-us/about-us'),
-    location: 'NYC',
-    tag: 1,
-  },
-  {
-    name: 'Taylor & CO',
-    link: new URL('https://www.taylorcobooks.com/'),
-    location: 'NYC',
-    tag: 0,
-  },
-  {
-    name: 'Seminary Co-op and 57th St Books',
-    link: new URL('http://www.semcoop.com'),
-    location: 'Chicago',
-    tag: 1,
-  },
-].map((b, i) => ({ ...b, id: i }))
-
-// interface GroupedOption {
-//   readonly label: string
-//   readonly options: Bookstore[]
-// }
-
-const groupedOptions = [
-  {
-    value: bookstores[0].link.href,
-    label: bookstores[0].name,
-  },
-  {
-    label: 'NYC',
-    options: bookstores
-      .filter((b) => b.location === 'NYC')
-      .map(({ name, link, ...data }) => ({
-        value: link.href,
-        label: name,
-        ...data,
-      })),
-  },
-  {
-    label: 'Chicago',
-    options: bookstores
-      .filter((b) => b.location === 'Chicago')
-      .map(({ name, link, ...data }) => ({
-        value: link.href,
-        label: name,
-        ...data,
-      })),
-  },
-]
-
-const locations = ['NYC', 'Chicago']
-
-// type GroupedOption = (typeof groupedOptions)[number]
-// type StoreOption = (typeof groupedOptions)[number]['options'][number]
-
 // const formatGroupLabel = (data: GroupedOption) => (
 //   <div className="text-gray-600">
 //     <span>{data.label}</span> <span>{data.options.length}</span>
 //   </div>
 // )
 
-const StoreSelect = () => {
-  const [selected, setSelected] = useState(0)
+const bookstoreMap = new Map<string, Bookstore>(
+  bookstores
+    .map<[string, Bookstore][]>((l, i) =>
+      l.stores.map((b, j) => [`${i},${j}`, b]),
+    )
+    .flat(),
+)
+
+interface StoreSelectProps {
+  onSelect: (store?: Bookstore) => void
+  focusElement?: HTMLElement | null
+}
+
+const StoreSelect = ({ onSelect, focusElement }: StoreSelectProps) => {
+  const [selected, setSelected] = useState(Array.from(bookstoreMap.keys())[0])
   return (
     <div className="absolute left-0 top-full flex w-full pt-1 text-sm">
       <span>from</span>
       <div className="ml-1">
         <Dropdown.Root>
           <Dropdown.Trigger className="text-amber-300 hover:underline">
-            {bookstores[selected].name || 'Select'}
+            {bookstoreMap.get(selected)?.name || 'Select'}
           </Dropdown.Trigger>
-          <Dropdown.Portal>
-            <Dropdown.Content
-              className="z-50 rounded-sm border border-gray-900 bg-white font-sans text-gray-900 drop-shadow"
-              // onCloseAutoFocus={(e) => e.preventDefault()} // should focus button
-            >
-              {locations.map((loc) => (
-                <Dropdown.Group className="divide-y divide-gray-100">
-                  <div className="bg-gray-100 px-2 py-1 text-xs font-semibold text-gray-600">
-                    {loc}
-                  </div>
-                  {bookstores
-                    .filter((b) => b.location === loc)
-                    .map(({ id, name, tag }) => (
-                      <Dropdown.Item
-                        key={id}
-                        className="cursor-pointer px-4 py-2 outline-none hover:bg-gray-50 focus:bg-gray-50"
-                        textValue={name}
-                        onSelect={() => setSelected(id)}
-                      >
-                        <div>{name}</div>
-                        {tag !== undefined && (
-                          <div className="ml-2 text-xs italic text-gray-500">
-                            {bookstoreTags[tag]}
-                          </div>
-                        )}
-                      </Dropdown.Item>
-                    ))}
-                </Dropdown.Group>
-              ))}
-              {/*bookstores.map((b) => (
-              <Dropdown.Item className="p-2">
-                <p>{b.name}</p>
-              </Dropdown.Item>
-            ))*/}
-            </Dropdown.Content>
-          </Dropdown.Portal>
+          <Dropdown.Content
+            className="z-50 max-h-96 overflow-y-scroll rounded-sm border border-gray-900 bg-white text-left font-sans text-gray-900 drop-shadow"
+            onCloseAutoFocus={(e) => {
+              if (!focusElement) return
+              e.preventDefault()
+              focusElement.focus()
+            }}
+          >
+            {bookstores.map(
+              (loc, i) =>
+                loc.stores.length > 0 && (
+                  <Dropdown.Group
+                    key={loc.name}
+                    className="divide-y divide-gray-100"
+                  >
+                    {loc.name && (
+                      <Dropdown.Label className="bg-gray-100 px-2 py-1 text-xs font-semibold text-gray-600">
+                        {loc.name}
+                      </Dropdown.Label>
+                    )}
+                    {loc.stores.map(({ name, tag }, j) => {
+                      const id = `${i},${j}`
+                      return (
+                        <Dropdown.Item
+                          key={id}
+                          className="cursor-pointer px-4 py-2 outline-none hover:bg-amber-50 focus:bg-amber-50"
+                          textValue={name}
+                          onSelect={() => {
+                            setSelected(id)
+                            onSelect(bookstoreMap.get(id))
+                          }}
+                        >
+                          <div>{name}</div>
+                          {tag && (
+                            <div className="ml-2 text-xs italic text-gray-400">
+                              {tag}
+                            </div>
+                          )}
+                        </Dropdown.Item>
+                      )
+                    })}
+                  </Dropdown.Group>
+                ),
+            )}
+          </Dropdown.Content>
         </Dropdown.Root>
       </div>
     </div>
   )
 }
 
-// const StoreSelect = () => (
-//   <div className="absolute left-0 top-full flex w-full pt-1 text-sm">
-//     <span>from</span>
-//     <Select<StoreOption, false, GroupedOption>
-//       options={groupedOptions}
-//       formatGroupLabel={formatGroupLabel}
-//       unstyled
-//       menuIsOpen={true}
-//       className="text-left font-sans text-gray-900"
-//       classNames={{
-//         menu: (state) => clsx('w-96 w-80 border-2 border-red-200'),
-//       }}
-//       styles={{
-//         menu: (baseStyles, state) => ({
-//           ...baseStyles,
-//           // border: 'red 2px solid',
-//           // width: '300px',
-//         }),
-//       }}
-//     />
-//   </div>
-// )
-
-// const StoreSelect = () => (
-//   <div className="absolute left-0 top-full flex w-full pt-1 text-sm">
-//     <span>from</span>
-//     <select
-//       name="bookstore"
-//       id="bookstore-select"
-//       className="ml-1 w-full cursor-pointer bg-transparent"
-//     >
-//       {bookstores.map(({ name, link, location, tag }) => (
-//         <option value={link.href}>
-//           <span>{name}</span>
-//           {tag && <span> ({bookstoreTags[tag]})</span>}
-//         </option>
-//       ))}
-//     </select>
-//   </div>
-// )
-
 const BuyTheBookLink = () => {
+  const button = useRef<HTMLAnchorElement>(null)
+  const [link, setLink] = useState(
+    'https://bookshop.org/p/books/the-human-origins-of-beatrice-porter-other-essential-ghosts-soraya-palmer/18592932',
+  )
   return (
     <div className="relative mr-2 inline-block">
       <a
-        href="https://bookshop.org/p/books/the-human-origins-of-beatrice-porter-other-essential-ghosts-soraya-palmer/18592932"
+        ref={button}
+        href={link}
         className="inline-block border-2 border-gray-500 px-4 py-3 font-sans"
         target="_blank"
       >
         Buy the book
       </a>
-      <StoreSelect />
+      <StoreSelect
+        onSelect={(store) => {
+          if (store) {
+            setLink(store.link.href)
+          }
+        }}
+        focusElement={button.current}
+      />
     </div>
   )
 }
