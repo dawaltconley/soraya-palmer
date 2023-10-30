821196adc8cd07374f460534c37e40221e717405
Dylan Awalt-Conley <web@dylan.ac>
2023-10-30 02:46:28 -0400

    WIP using radix

--- src/components/BookSplash.tsx
+++ src/components/BookSplash.tsx
@@ -4,6 +4,8 @@ import type {
   CSSProperties,
 } from 'react'
 import { useState, useEffect, useRef } from 'react'
+import Select from 'react-select'
+import * as Dropdown from '@radix-ui/react-dropdown-menu'
 import Icon from './Icon'
 import Spinner from './Spinner'
 import clsx from 'clsx'
@@ -59,7 +61,7 @@ const Trailer = ({
 
 const bookstoreTags = [
   'Black owned',
-  'worker or customer owned',
+  'worker/customer owned',
   'Black women owned',
 ] as const
 
@@ -67,7 +69,9 @@ interface Bookstore {
   name: string
   link: URL
   location?: string
-  tag?: 0 | 1 | 2
+  // tag?: 0 | 1 | 2
+  tag?: number
+  id: number
 }
 
 const bookstores: Bookstore[] = [
@@ -107,10 +111,142 @@ const bookstores: Bookstore[] = [
     location: 'Chicago',
     tag: 1,
   },
+].map((b, i) => ({ ...b, id: i }))
+
+// interface GroupedOption {
+//   readonly label: string
+//   readonly options: Bookstore[]
+// }
+
+const groupedOptions = [
+  {
+    value: bookstores[0].link.href,
+    label: bookstores[0].name,
+  },
+  {
+    label: 'NYC',
+    options: bookstores
+      .filter((b) => b.location === 'NYC')
+      .map(({ name, link, ...data }) => ({
+        value: link.href,
+        label: name,
+        ...data,
+      })),
+  },
+  {
+    label: 'Chicago',
+    options: bookstores
+      .filter((b) => b.location === 'Chicago')
+      .map(({ name, link, ...data }) => ({
+        value: link.href,
+        label: name,
+        ...data,
+      })),
+  },
 ]
 
-// const bookstoreOptions = new Map<string, URL>(
-//   []
+const locations = ['NYC', 'Chicago']
+
+// type GroupedOption = (typeof groupedOptions)[number]
+// type StoreOption = (typeof groupedOptions)[number]['options'][number]
+
+// const formatGroupLabel = (data: GroupedOption) => (
+//   <div className="text-gray-600">
+//     <span>{data.label}</span> <span>{data.options.length}</span>
+//   </div>
+// )
+
+const StoreSelect = () => {
+  const [selected, setSelected] = useState(0)
+  return (
+    <div className="absolute left-0 top-full flex w-full pt-1 text-sm">
+      <span>from</span>
+      <div className="ml-1">
+        <Dropdown.Root>
+          <Dropdown.Trigger className="text-amber-300 hover:underline">
+            {bookstores[selected].name || 'Select'}
+          </Dropdown.Trigger>
+          <Dropdown.Portal>
+            <Dropdown.Content
+              className="z-50 rounded-sm border border-gray-900 bg-white font-sans text-gray-900 drop-shadow"
+              // onCloseAutoFocus={(e) => e.preventDefault()} // should focus button
+            >
+              {locations.map((loc) => (
+                <Dropdown.Group className="divide-y divide-gray-100">
+                  <div className="bg-gray-100 px-2 py-1 text-xs font-semibold text-gray-600">
+                    {loc}
+                  </div>
+                  {bookstores
+                    .filter((b) => b.location === loc)
+                    .map(({ id, name, tag }) => (
+                      <Dropdown.Item
+                        key={id}
+                        className="cursor-pointer px-4 py-2 outline-none hover:bg-gray-50 focus:bg-gray-50"
+                        textValue={name}
+                        onSelect={() => setSelected(id)}
+                      >
+                        <div>{name}</div>
+                        {tag !== undefined && (
+                          <div className="ml-2 text-xs italic text-gray-500">
+                            {bookstoreTags[tag]}
+                          </div>
+                        )}
+                      </Dropdown.Item>
+                    ))}
+                </Dropdown.Group>
+              ))}
+              {/*bookstores.map((b) => (
+              <Dropdown.Item className="p-2">
+                <p>{b.name}</p>
+              </Dropdown.Item>
+            ))*/}
+            </Dropdown.Content>
+          </Dropdown.Portal>
+        </Dropdown.Root>
+      </div>
+    </div>
+  )
+}
+
+// const StoreSelect = () => (
+//   <div className="absolute left-0 top-full flex w-full pt-1 text-sm">
+//     <span>from</span>
+//     <Select<StoreOption, false, GroupedOption>
+//       options={groupedOptions}
+//       formatGroupLabel={formatGroupLabel}
+//       unstyled
+//       menuIsOpen={true}
+//       className="text-left font-sans text-gray-900"
+//       classNames={{
+//         menu: (state) => clsx('w-96 w-80 border-2 border-red-200'),
+//       }}
+//       styles={{
+//         menu: (baseStyles, state) => ({
+//           ...baseStyles,
+//           // border: 'red 2px solid',
+//           // width: '300px',
+//         }),
+//       }}
+//     />
+//   </div>
+// )
+
+// const StoreSelect = () => (
+//   <div className="absolute left-0 top-full flex w-full pt-1 text-sm">
+//     <span>from</span>
+//     <select
+//       name="bookstore"
+//       id="bookstore-select"
+//       className="ml-1 w-full cursor-pointer bg-transparent"
+//     >
+//       {bookstores.map(({ name, link, location, tag }) => (
+//         <option value={link.href}>
+//           <span>{name}</span>
+//           {tag && <span> ({bookstoreTags[tag]})</span>}
+//         </option>
+//       ))}
+//     </select>
+//   </div>
 // )
 
 const BuyTheBookLink = () => {
@@ -123,21 +259,7 @@ const BuyTheBookLink = () => {
       >
         Buy the book
       </a>
-      <div className="absolute left-0 top-full flex w-full pt-1 text-sm">
-        <span>from</span>
-        <select
-          name="bookstore"
-          id="bookstore-select"
-          className="ml-1 w-full cursor-pointer bg-transparent"
-        >
-          {bookstores.map(({ name, link, location, tag }) => (
-            <option value={link.href}>
-              <span>{name}</span>
-              {tag && <span> ({bookstoreTags[tag]})</span>}
-            </option>
-          ))}
-        </select>
-      </div>
+      <StoreSelect />
     </div>
   )
 }
