7b31ce498825f78a712ae125b0ec186e0e31209e
Dylan Awalt-Conley <web@dylan.ac>
2023-10-29 20:46:38 -0400

    basic ContactForm component

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,10 +1,6 @@
-import type {
-  FunctionComponent,
-  ReactNode,
-  FormEvent,
-  ComponentPropsWithoutRef,
-} from 'react'
+import type { FunctionComponent, ReactNode, FormEvent } from 'react'
 import { useState, useEffect, useRef } from 'react'
+import { submitForm } from '@browser/forms'
 import clsx from 'clsx'
 import Spinner from './Spinner'
 
@@ -14,56 +10,6 @@ const requiredFields = ['name', 'email', 'subject', 'message'] as const
 
 const isValid = (data: FormData): boolean =>
   requiredFields.every((field) => data.get(field))
-
-const copyFormData = (data: FormData): FormData => {
-  const copy = new FormData()
-  for (const [key, value] of data.entries()) {
-    copy.append(key, value)
-  }
-  return copy
-}
-
-const formDataToSearchParams = (formData: FormData): URLSearchParams => {
-  const formEntries = Array.from(formData, ([key, value]) => [
-    key,
-    typeof value === 'string' ? value : value.name,
-  ])
-  return new URLSearchParams(formEntries)
-}
-
-interface SubmitFormOps {
-  action: string | URL
-  method?: 'GET' | 'POST'
-  encType?: 'application/x-www-form-urlencoded' | 'multipart/form-data'
-}
-
-const submitForm = (
-  data: FormData,
-  { action, method, encType }: Required<SubmitFormOps>,
-): Promise<Response> => {
-  const target = new URL(action)
-  const opts: RequestInit = { method, redirect: 'follow' }
-  const dataCopy = copyFormData(data)
-  const subject = dataCopy.get('subject')
-  if (subject) dataCopy.set('subject', `[${window.location.host}] ${subject}`)
-
-  if (method === 'GET') {
-    if (encType === 'multipart/form-data')
-      throw new Error(
-        'Unupported: GET method with multipart/form-data encoding',
-      )
-    target.search = formDataToSearchParams(dataCopy).toString()
-  } else if (method === 'POST') {
-    opts.body =
-      encType === 'application/x-www-form-urlencoded'
-        ? formDataToSearchParams(dataCopy)
-        : dataCopy
-  } else {
-    throw new Error(`Invalid ${method} request method`)
-  }
-
-  return fetch(target, opts)
-}
 
 interface ContactFormProps {
   action: string | URL
@@ -99,13 +45,15 @@ export default function ContactForm({
     if (!container || !form) return
 
     container.style.minHeight = `${container.clientHeight.toString()}px`
-    setStatus('submitting')
 
     const data = new FormData(form)
     if (!isValid(data)) {
       return handleError('Missing required fields in contact form')
     }
     formData.current = data
+
+    setStatus('submitting')
+    await new Promise((resolve) => window.setTimeout(resolve, 2000))
 
     try {
       const response = await submitForm(data, { action, method, encType })
@@ -153,87 +101,75 @@ export default function ContactForm({
       {showForm && (
         <form
           ref={formRef}
-          className="contact-form mx-auto mt-8 w-full max-w-xl grid-cols-2 text-lg"
+          className="contact-form mx-auto mt-8 w-full max-w-xl grid-cols-2 text-lg @container/contact-form"
           method={method.toLowerCase()}
           encType={encType}
           action={action.toString()}
           onSubmit={handleSubmit}
         >
-          <label htmlFor="contact-name" className="form-label sm:col-span-1">
-            <span className="form-label__text">Name</span>
+          <label htmlFor="contact-name" className="form-label @md:col-span-1">
             <input
               id="contact-name"
               name="name"
               type="text"
               className="form-field w-full"
               autoComplete="name"
+              disabled={status === 'submitting'}
+              required
             />
+            <span className="form-label__text">Name</span>
           </label>
-          <label htmlFor="contact-email" className="form-label sm:col-span-1">
-            <span className="form-label__text">Email</span>
+          <label htmlFor="contact-email" className="form-label @md:col-span-1">
             <input
               id="contact-email"
               name="email"
               type="email"
               className="form-field w-full"
               autoComplete="email"
+              disabled={status === 'submitting'}
+              required
             />
+            <span className="form-label__text">Email</span>
           </label>
           <label htmlFor="contact-subject" className="form-label">
-            <span className="form-label__text">Subject</span>
             <input
               id="contact-subject"
               name="subject"
               type="text"
               className="form-field w-full"
+              disabled={status === 'submitting'}
+              required
             />
+            <span className="form-label__text">Subject</span>
           </label>
           <label htmlFor="contact-message" className="form-label">
-            <span className="form-label__text">Message</span>
             <textarea
               id="contact-message"
               name="message"
               className="form-field min-h-[12rem] w-full"
+              disabled={status === 'submitting'}
+              required
             ></textarea>
+            <span className="form-label__text">Message</span>
           </label>
-          <button type="submit" className="form-button col-span-2">
-            {status === 'submitting' ? <Spinner /> : 'Send'}
+          <button
+            type="submit"
+            className="form-button col-span-2 text-base @lg:text-lg"
+            disabled={status === 'submitting'}
+          >
+            {status === 'submitting' ? (
+              <>
+                <Spinner /> Sending
+              </>
+            ) : (
+              'Send'
+            )}
           </button>
         </form>
       )}
     </div>
   )
 }
-
-// type LabeledInputProps<T extends 'input' | 'textarea' = 'input' | 'textarea'> =
-//   {
-//     id: string
-//     label: string
-//   } & ComponentPropsWithoutRef<T>
-//
-// const LabeledInput = ({
-//   id,
-//   label,
-//   className,
-//   ...props
-// }: LabeledInputProps<'input'>) => (
-//   <label htmlFor={id} className={clsx('form-label', className)}>
-//     <span className="form-label__text">{label}</span>
-//     <input id={id} className="form-field w-full" {...props} />
-//   </label>
-// )
-//
-// const LabeledTextArea = ({
-//   id,
-//   label,
-//   className,
-//   ...props
-// }: LabeledInputProps<'textarea'>) => (
-//   <label htmlFor={id} className={clsx('form-label', className)}>
-//     <span className="form-label__text">{label}</span>
-//     <textarea id={id} className="form-field min-h-[12rem] w-full" {...props} />
-//   </label>
-// )
 
 interface FormMessageProps {
   status: ContactFormStatus
@@ -246,7 +182,7 @@ const FormMessage: FunctionComponent<FormMessageProps> = ({
 }) => {
   const title: Record<typeof status, string | JSX.Element> = {
     initial: 'Contact me',
-    submitting: 'Sending...',
+    submitting: 'Contact me',
     error: 'Something went wrong',
     success: 'Thank you!',
   }
@@ -263,20 +199,18 @@ const FormMessage: FunctionComponent<FormMessageProps> = ({
       <h2 className="mb-4 text-center font-display text-3xl font-bold capitalize">
         {title[status]}
       </h2>
-      <div
-        className={clsx('font-serif font-medium', {
-          'min-h-[6rem]': status === 'submitting' || status === 'success',
-        })}
-      >
-        {message}
-      </div>
+      {message && (
+        <div
+          className={clsx('font-serif font-medium', {
+            'min-h-[6rem]': status === 'submitting' || status === 'success',
+          })}
+        >
+          {message}
+        </div>
+      )}
     </>
   )
 }
-
-const InitialMessage = () => null
-
-const SendingMessage = () => <Spinner className="text-6xl" />
 
 const ErrorMessage: FunctionComponent<{ message?: string }> = ({ message }) =>
   message && (
