3b9f419480e2cc589fafee576e0e7feafec21f5e
Dylan Awalt-Conley <web@dylan.ac>
2023-10-30 18:23:42 -0400

    separate BookStoreSelect into own component

--- src/components/BookSplash.tsx
+++ src/components/BookSplash.tsx
@@ -4,14 +4,12 @@ import type {
   CSSProperties,
 } from 'react'
 import { useState, useEffect, useRef } from 'react'
-import * as Dropdown from '@radix-ui/react-dropdown-menu'
 import Icon from './Icon'
 import Spinner from './Spinner'
 import clsx from 'clsx'
 import { faCirclePlay } from '@fortawesome/pro-regular-svg-icons/faCirclePlay'
 import { faXmark } from '@fortawesome/pro-regular-svg-icons/faXmark'
-import type { Bookstore } from '@data/bookstores'
-import bookstores from '@data/bookstores'
+import BookStoreSelect from './BookStoreSelect'
 
 interface TrailerProps extends ComponentPropsWithoutRef<'video'> {
   play?: boolean
@@ -60,80 +58,6 @@ const Trailer = ({
   )
 }
 
-const bookstoreMap = new Map<string, Bookstore>(
-  bookstores
-    .map<[string, Bookstore][]>((l, i) =>
-      l.stores.map((b, j) => [`${i},${j}`, b]),
-    )
-    .flat(),
-)
-
-interface StoreSelectProps {
-  onSelect: (store?: Bookstore) => void
-  focusElement?: HTMLElement | null
-}
-
-const StoreSelect = ({ onSelect, focusElement }: StoreSelectProps) => {
-  const [selected, setSelected] = useState(Array.from(bookstoreMap.keys())[0])
-  return (
-    <div className="absolute left-0 top-full flex w-full pt-1 text-sm">
-      <span>from</span>
-      <div className="ml-1">
-        <Dropdown.Root>
-          <Dropdown.Trigger className="text-amber-300 hover:underline">
-            {bookstoreMap.get(selected)?.name || 'Select'}
-          </Dropdown.Trigger>
-          <Dropdown.Content
-            className="z-50 max-h-96 overflow-y-scroll rounded-sm border border-gray-900 bg-white text-left font-sans text-gray-900 drop-shadow"
-            onCloseAutoFocus={(e) => {
-              if (!focusElement) return
-              e.preventDefault()
-              focusElement.focus()
-            }}
-          >
-            {bookstores.map(
-              (loc, i) =>
-                loc.stores.length > 0 && (
-                  <Dropdown.Group
-                    key={loc.name}
-                    className="divide-y divide-gray-100"
-                  >
-                    {loc.name && (
-                      <Dropdown.Label className="bg-gray-100 px-2 py-1 text-xs font-semibold text-gray-600">
-                        {loc.name}
-                      </Dropdown.Label>
-                    )}
-                    {loc.stores.map(({ name, tag }, j) => {
-                      const id = `${i},${j}`
-                      return (
-                        <Dropdown.Item
-                          key={id}
-                          className="cursor-pointer px-4 py-2 outline-none hover:bg-amber-50 focus:bg-amber-50"
-                          textValue={name}
-                          onSelect={() => {
-                            setSelected(id)
-                            onSelect(bookstoreMap.get(id))
-                          }}
-                        >
-                          <div>{name}</div>
-                          {tag && (
-                            <div className="ml-2 text-xs italic text-gray-400">
-                              {tag}
-                            </div>
-                          )}
-                        </Dropdown.Item>
-                      )
-                    })}
-                  </Dropdown.Group>
-                ),
-            )}
-          </Dropdown.Content>
-        </Dropdown.Root>
-      </div>
-    </div>
-  )
-}
-
 const BuyTheBookLink = () => {
   const button = useRef<HTMLAnchorElement>(null)
   const [link, setLink] = useState(
@@ -149,7 +73,7 @@ const BuyTheBookLink = () => {
       >
         Buy the book
       </a>
-      <StoreSelect
+      <BookStoreSelect
         onSelect={(store) => {
           if (store) {
             setLink(store.link.href)
---
+++ src/components/BookStoreSelect.tsx
@@ -0,0 +1,81 @@
+import type { Bookstore } from '@data/bookstores'
+import bookstores from '@data/bookstores'
+import { useState } from 'react'
+import * as Dropdown from '@radix-ui/react-dropdown-menu'
+
+const bookstoreMap = new Map<string, Bookstore>(
+  bookstores
+    .map<[string, Bookstore][]>((l, i) =>
+      l.stores.map((b, j) => [`${i},${j}`, b]),
+    )
+    .flat(),
+)
+
+interface StoreSelectProps {
+  onSelect: (store?: Bookstore) => void
+  focusElement?: HTMLElement | null
+}
+
+export default function BookStoreSelect({
+  onSelect,
+  focusElement,
+}: StoreSelectProps) {
+  const [selected, setSelected] = useState(Array.from(bookstoreMap.keys())[0])
+  return (
+    <div className="absolute left-0 top-full flex w-full pt-1 text-sm">
+      <span>from</span>
+      <div className="ml-1">
+        <Dropdown.Root>
+          <Dropdown.Trigger className="text-amber-300 hover:underline">
+            {bookstoreMap.get(selected)?.name || 'Select'}
+          </Dropdown.Trigger>
+          <Dropdown.Content
+            className="z-50 max-h-96 overflow-y-scroll rounded-sm border border-gray-900 bg-white text-left font-sans text-gray-900 drop-shadow"
+            onCloseAutoFocus={(e) => {
+              if (!focusElement) return
+              e.preventDefault()
+              focusElement.focus()
+            }}
+          >
+            {bookstores.map(
+              (loc, i) =>
+                loc.stores.length > 0 && (
+                  <Dropdown.Group
+                    key={loc.name}
+                    className="divide-y divide-gray-100"
+                  >
+                    {loc.name && (
+                      <Dropdown.Label className="bg-gray-100 px-2 py-1 text-xs font-semibold text-gray-600">
+                        {loc.name}
+                      </Dropdown.Label>
+                    )}
+                    {loc.stores.map(({ name, tag }, j) => {
+                      const id = `${i},${j}`
+                      return (
+                        <Dropdown.Item
+                          key={id}
+                          className="cursor-pointer px-4 py-2 outline-none hover:bg-amber-50 focus:bg-amber-50"
+                          textValue={name}
+                          onSelect={() => {
+                            setSelected(id)
+                            onSelect(bookstoreMap.get(id))
+                          }}
+                        >
+                          <div>{name}</div>
+                          {tag && (
+                            <div className="ml-2 text-xs italic text-gray-400">
+                              {tag}
+                            </div>
+                          )}
+                        </Dropdown.Item>
+                      )
+                    })}
+                  </Dropdown.Group>
+                ),
+            )}
+          </Dropdown.Content>
+        </Dropdown.Root>
+      </div>
+    </div>
+  )
+}
