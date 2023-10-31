6fabe3934be90597933f72cef9145f785f2d799c
Dylan Awalt-Conley <web@dylan.ac>
2023-10-30 21:56:09 -0400

    separate component for BookBuyButton

--- src/components/BookSplash.tsx
+++ src/components/BookSplash.tsx
@@ -10,7 +10,7 @@ import clsx from 'clsx'
 import { faCirclePlay } from '@fortawesome/pro-regular-svg-icons/faCirclePlay'
 import { faArrowUpRightFromSquare } from '@fortawesome/pro-regular-svg-icons/faArrowUpRightFromSquare'
 import { faXmark } from '@fortawesome/pro-regular-svg-icons/faXmark'
-import BookStoreSelect from './BookStoreSelect'
+import BuyTheBookLink from './BookBuyButton'
 
 interface TrailerProps extends ComponentPropsWithoutRef<'video'> {
   play?: boolean
@@ -56,38 +56,6 @@ const Trailer = ({
     >
       <source src="/media/humanorigins_1080x1080_3.mp4" type="video/mp4" />
     </video>
-  )
-}
-
-const BuyTheBookLink = () => {
-  const button = useRef<HTMLAnchorElement>(null)
-  const [link, setLink] = useState(
-    'https://bookshop.org/p/books/the-human-origins-of-beatrice-porter-other-essential-ghosts-soraya-palmer/18592932',
-  )
-  return (
-    <div className="relative items-center justify-stretch font-sans @md:flex">
-      <a
-        ref={button}
-        href={link}
-        className="group inline-block border-2 border-amber-300/80 bg-gray-950/60 px-4 py-3 font-sans duration-300 hover:border-amber-300 hover:bg-gray-950 focus-visible:border-amber-300 focus-visible:bg-gray-950"
-        target="_blank"
-      >
-        <span className="underline-link group-hover:underline-link--active group-focus-visible:underline-link--active whitespace-nowrap font-semibold">
-          Buy the book
-        </span>
-      </a>
-      <div className="flex h-full w-full items-baseline border-transparent @md:ml-4">
-        <span>from</span>
-        <BookStoreSelect
-          onSelect={(store) => {
-            if (store) {
-              setLink(store.link.href)
-            }
-          }}
-          focusElement={button.current}
-        />
-      </div>
-    </div>
   )
 }
 
