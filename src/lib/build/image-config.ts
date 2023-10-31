4111c4959905609c65d508bb179f2600b1b689a5
Dylan Awalt-Conley <web@dylan.ac>
2023-10-31 04:10:37 -0400

    use MODE env variable, seems more reliable?

--- src/lib/build/image-config.ts
+++ src/lib/build/image-config.ts
@@ -1,13 +1,15 @@
-import { ResponsiveImages } from '@dawaltconley/responsive-images';
+import { ResponsiveImages } from '@dawaltconley/responsive-images'
+
+const isProduction = import.meta.env.MODE === 'production'
 
 export default new ResponsiveImages({
   scalingFactor: 0.5,
   defaults: {
     formats: ['webp', null],
     outputDir: './dist/_responsive-images/',
-    urlPath: import.meta.env.PROD
+    urlPath: isProduction
       ? '/_responsive-images/'
       : '/dist/_responsive-images/',
   },
-  disable: !import.meta.env.PROD,
-});
+  disable: !isProduction,
+})
