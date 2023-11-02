51bacc436db5aa1448573e9ab9ff11af8371157a
Dylan Awalt-Conley <web@dylan.ac>
2023-11-02 01:09:56 -0400

    make home page writing sample images responsive

--- src/components/WritingSamples.tsx
+++ src/components/WritingSamples.tsx
@@ -1,42 +1,54 @@
 import type { WritingSamplesQuery } from '@tina/__generated__/types'
+import type { ResponsiveImageData } from '@build/images'
 import { tinaField } from 'tinacms/dist/react'
 import { TinaMarkdown } from 'tinacms/dist/rich-text'
 import { withTinaWrapper } from '@browser/withTinaWrapper'
 import ArticlePreview from './ArticlePreview'
 import { isNotEmpty } from '@lib/utils'
+import { getMetadata } from '@lib/images'
 
-export default withTinaWrapper<WritingSamplesQuery>(({ data }) => {
-  const { title, selections } = data.home.writing
-  const articles = selections?.map((w) => w?.item).filter(isNotEmpty)
-  return (
-    <div
-      id="writing"
-      className="bg-gray-50 py-16"
-      data-tina-field={tinaField(data.home.writing)}
-    >
-      <div className="container mx-auto">
-        <h2 className="mb-8 text-center font-display text-3xl font-bold">
-          {title}
-        </h2>
-        {articles && (
-          <div className="grid gap-8 xl:grid-cols-3">
-            {articles.slice(0, 3).map(({ description, ...article }) => {
-              description = description?.children?.length ? (
-                <TinaMarkdown content={description} />
-              ) : null
-              return (
-                <ArticlePreview
-                  key={article.url}
-                  style="inline"
-                  layout="date"
-                  description={description}
-                  {...article}
-                />
-              )
-            })}
-          </div>
-        )}
+interface WritingSamplesProps {
+  images?: ResponsiveImageData
+}
+
+export default withTinaWrapper<WritingSamplesQuery, WritingSamplesProps>(
+  ({ data, images = {} }) => {
+    const { title, selections } = data.home.writing
+    const articles = selections?.map((w) => w?.item).filter(isNotEmpty)
+    return (
+      <div
+        id="writing"
+        className="bg-gray-50 py-16"
+        data-tina-field={tinaField(data.home.writing)}
+      >
+        <div className="container mx-auto">
+          <h2 className="mb-8 text-center font-display text-3xl font-bold">
+            {title}
+          </h2>
+          {articles && (
+            <div className="grid gap-8 xl:grid-cols-3">
+              {articles
+                .slice(0, 3)
+                .map(({ description, image, ...article }) => {
+                  description = description?.children?.length ? (
+                    <TinaMarkdown content={description} />
+                  ) : null
+                  const metadata = getMetadata(image, images)
+                  return (
+                    <ArticlePreview
+                      key={article.url}
+                      style="inline"
+                      layout="date"
+                      image={metadata}
+                      description={description}
+                      {...article}
+                    />
+                  )
+                })}
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  )
-})
+    )
+  },
+)
