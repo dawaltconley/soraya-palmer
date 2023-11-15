01ab1b98bc0a57aa20f2e1825f9181f5126a62b0
Dylan Awalt-Conley <web@dylan.ac>
2023-11-14 21:39:00 -0500

    simplify icon imports

--- src/data/icons.ts
+++ src/data/icons.ts
@@ -1,10 +1,10 @@
 /* eslint-disable no-duplicate-imports, @typescript-eslint/no-duplicate-imports */
 
-import { definition as IconEmail } from '@fortawesome/pro-regular-svg-icons/faEnvelope'
-import { definition as IconPhone } from '@fortawesome/pro-regular-svg-icons/faPhone'
-import { definition as IconTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter'
-import { definition as IconLinkedIn } from '@fortawesome/free-brands-svg-icons/faLinkedin'
-import { definition as IconInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram'
+import { faEnvelope } from '@fortawesome/pro-regular-svg-icons/faEnvelope'
+import { faPhone } from '@fortawesome/pro-regular-svg-icons/faPhone'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram'
 import { faTiktok } from '@fortawesome/free-brands-svg-icons/faTiktok'
 import { faFacebook } from '@fortawesome/free-brands-svg-icons/faFacebook'
 import { faYoutube } from '@fortawesome/free-brands-svg-icons/faYoutube'
@@ -12,22 +12,22 @@ import { faYoutube } from '@fortawesome/free-brands-svg-icons/faYoutube'
 import { Icon, getIconFromUrl } from '@lib/icons'
 
 const icons = {
-  email: new Icon({ name: 'Email', url: 'mailto:', icons: IconEmail }),
-  phone: new Icon({ name: 'Phone', url: 'tel:', icons: IconPhone }),
+  email: new Icon({ name: 'Email', url: 'mailto:', icons: faEnvelope }),
+  phone: new Icon({ name: 'Phone', url: 'tel:', icons: faPhone }),
   twitter: new Icon({
     name: 'Twitter',
     url: 'https://twitter.com',
-    icons: IconTwitter,
+    icons: faTwitter,
   }),
   linkedin: new Icon({
     name: 'LinkedIn',
     url: 'https://www.linkedin.com/',
-    icons: IconLinkedIn,
+    icons: faLinkedin,
   }),
   instagram: new Icon({
     name: 'Instagram',
     url: 'https://www.instagram.com/',
-    icons: IconInstagram,
+    icons: faInstagram,
   }),
   tiktok: new Icon({
     name: 'TikTok',
