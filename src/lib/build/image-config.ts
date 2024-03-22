951b86809338c790498e59bda96d7aa64bd3bcca
Dylan Awalt-Conley <web@dylan.ac>
2024-03-21 23:12:40 -0400

    update responsive images and add optimizations
    Ignore devices larger than 3dpr. automatically fix image rotations.

--- src/lib/build/image-config.ts
+++ src/lib/build/image-config.ts
@@ -1,8 +1,10 @@
 import { ResponsiveImages } from '@dawaltconley/responsive-images'
+import { devices } from '../../data/devices'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 const config = new ResponsiveImages({
+  devices,
   scalingFactor: 0.5,
   defaults: {
     formats: ['webp', null],
@@ -10,7 +12,7 @@ const config = new ResponsiveImages({
     urlPath: isProduction
       ? '/_responsive-images/'
       : '/dist/_responsive-images/',
-    // fixOrientation: true,
+    fixOrientation: true,
   },
   disable: !isProduction,
 })
